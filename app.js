23a3557f5638deb265555cc6dfc8b8f95f8ce154
Laurence Newman <ln0142@Laurences-MBP.home>
2016-10-08 08:35:04 +0100

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var queue = kue.createQueue();
 //connect to redis 
 var redis = require('redis');
 var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var redisURL = url.parse(process.env.REDIS_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
